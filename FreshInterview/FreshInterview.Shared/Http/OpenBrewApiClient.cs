b40caa4ae678a9901b15ad0b5a373740716d3082
RynBelz162 <rynbelz162@gmail.com>
2022-12-09 17:58:05 -0500

    🔨 add missing files

---
+++ FreshInterview/FreshInterview.Shared/Http/OpenBrewApiClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FreshInterview.Shared.Models;
+
+namespace FreshInterview.Shared.Http
+{
+	public class OpenBrewApiClient
+	{
+        private readonly HttpClient _httpClient;
+        private JsonSerializerOptions JsonOptions => new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+
+        public OpenBrewApiClient(HttpClient httpClient)
+		{
+			_httpClient = httpClient;
+			_httpClient.BaseAddress = new Uri("https://api.openbrewerydb.org/breweries");
+		}
+
+		public async Task<List<Brewery>> GetBreweries(int page = 1, int take = 20)
+		{
+			var request = await _httpClient.GetAsync($"?page={page}&per_page={take}");
+            request.EnsureSuccessStatusCode();
+
+			var rawResult = await request.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<List<Brewery>>(rawResult, JsonOptions);
+        }
+    }
+}
+
---
+++ FreshInterview/FreshInterview.Shared/Models/Brewery.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace FreshInterview.Shared.Models
+{
+    public class Brewery
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        [JsonPropertyName("brewery_type")]
+        public string BreweryType { get; set; }
+
+        public string Street { get; set; }
+
+        [JsonPropertyName("address_2")]
+        public string Address2 { get; set; }
+
+        [JsonPropertyName("address_3")]
+        public string Address3 { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        [JsonPropertyName("county_province")]
+        public string CountyProvince { get; set; }
+
+        [JsonPropertyName("postal_code")]
+        public string PostalCode { get; set; }
+
+        public string Country { get; set; }
+
+        public string Longitude { get; set; }
+
+        public string Latitude { get; set; }
+
+        public string Phone { get; set; }
+
+        [JsonPropertyName("website_url")]
+        public string WebsiteUrl { get; set; }
+
+        [JsonPropertyName("updated_at")]
+        public DateTime UpdatedAt { get; set; }
+
+        [JsonPropertyName("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        public string FormattedAddress => $"{Street} {City}, {State} {PostalCode}";
+    }
+
+}
+
